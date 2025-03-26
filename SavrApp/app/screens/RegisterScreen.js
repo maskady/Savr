c2b876230b8b0a9b2ce0b459627d3ec3729c8cc4
Maskady <florent.tout@gmail.com>
2025-03-26 15:23:13 +0200

    Add back navigation button to LoginScreen and RegisterScreen for iOS

--- SavrApp/app/screens/RegisterScreen.js
+++ SavrApp/app/screens/RegisterScreen.js
@@ -8,9 +8,11 @@ import {
   TouchableOpacity,
   StatusBar,
   Appearance,
+  Platform,
 } from "react-native";
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { storeToken } from "../utils/token";
+import { ArrowLeft } from "lucide-react-native";
 
 const RegisterScreen = ({setIsAuthenticated}) => {
   const [fullName, setFullName] = useState("");
@@ -90,6 +92,11 @@ const RegisterScreen = ({setIsAuthenticated}) => {
         backgroundColor={isDarkMode ? "#333" : "white"}
       />
       <View style={styles.content}>
+        {Platform.OS === "ios" && (
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <ArrowLeft size={24} color={isDarkMode ? "white" : "black"} />
+          </TouchableOpacity>
+        )}
         <Text style={[styles.title, { color: isDarkMode ? "white" : "black" }]}>
           Savr
         </Text>
