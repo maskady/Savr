da3edfbd48053a5cbf48f902f745f239aeffd5c1
Maskady <florent.tout@gmail.com>
2025-03-26 12:19:08 +0200

    Add navigation structure with authentication and main app stacks

---
+++ SavrApp/app/navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import AuthStack from "./AuthStack";
+import AppStack from "./AppStack";
+
+const Navigation = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+    isAuthenticated ? <AppStack /> : <AuthStack />
+  );
+};
+
+export default Navigation;
