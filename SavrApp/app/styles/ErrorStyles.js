2fcb9099243953f3987e16cfd3735b9f1a46643a
Maskady <florent.tout@gmail.com>
2025-04-01 18:09:36 +0300

    Refactor ErrorScreen styles: move styles to ErrorStyles file and remove inline styles

---
+++ SavrApp/app/styles/ErrorStyles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+    padding: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 10,
+  },
+  message: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#4A90E2',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 25,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 16,
+  },
+});
+
+export default styles;
