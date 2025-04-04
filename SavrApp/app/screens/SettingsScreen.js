28cc42ff515fb87656d451aca623d0e36384451b
Maskady <florent.tout@gmail.com>
2025-04-04 18:07:33 +0300

    add user data fetching in SettingsScreen component

--- SavrApp/app/screens/SettingsScreen.js
+++ SavrApp/app/screens/SettingsScreen.js
@@ -1,7 +1,44 @@
-import React from 'react';
+  const navigation = useNavigation();
 
-const SettingsScreen = ( ) => {
-  
+  useEffect(() => {
+    const loadUserData = async () => {
+      try {
+        // TODO : Replace with our API endpoint
+        // const response = await fetch("", {
+        //   method: "GET",
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //     Authorization: `Bearer ${await getToken()}`,
+        //   },
+        // });
+        // const data = await response.json();
+
+        // Fake response for demonstration purposes
+        const response = {
+          data: {
+            role: "user",
+            firstName: "John",
+            lastName: "Doe",
+            email: "john.doe@domain.com",
+          },
+          ok: true,
+        }
+        const data = response.data;
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+
+        console.log("User data:", data);
+        setUser(data);
+        setFirstName(data.firstName);
+        setLastName(data.lastName);
+        setEmail(data.email);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        navigation.navigate("Error", { error: "Failed to load user data" });
+      }
+    };
 };
 
 export default SettingsScreen;
