4e141b7dddff6bdab754b749bccf42457dfa01ee
Space0code <bozak.tomi@gmail.com>
2025-04-08 12:21:28 +0300

    quick ugly fix to console.log only first n lines on error for readability

--- SavrApp/app/index.js
+++ SavrApp/app/index.js
@@ -4,6 +4,19 @@ import { AuthProvider } from './contexts/AuthContext';
 import { SettingsProvider } from "./contexts/SettingsContext";
 
 export default function App() {
+  // Show only the first line of console.error messages
+  // This is useful for debugging, as it prevents long error messages from cluttering the console.
+  const originalConsoleError = console.error;
+  console.error = (...args) => {
+    if (typeof args[0] === 'string') {
+      const n_lines = 10
+      const firstLines = args[0].split('\n').slice(0, n_lines).join('\n'); // Get only the first n lines
+      originalConsoleError(firstLines);
+    } else {
+      originalConsoleError(...args);
+    }
+  };
+
   return (
     <AuthProvider>
       <SettingsProvider>
