98a8c12c04e8647b0cee666a8eedd5ddb1cd1238
Maskady <florent.tout@gmail.com>
2025-04-11 16:12:41 +0300

    Refactor Navigation component to manage initial route based on authentication state

--- SavrApp/app/navigation/Navigation.js
+++ SavrApp/app/navigation/Navigation.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { AuthContext } from "../contexts/AuthContext";
 
 import AuthStack from "./AuthStack";
 import AppStack from "./AppStack";
@@ -7,8 +8,22 @@ import AppStack from "./AppStack";
 const RootStack = createStackNavigator();
 
 const Navigation = () => {
+  const { isLoggedIn } = useContext(AuthContext);
+  const [routeName, setRouteName] = useState(isLoggedIn ? "App" : "Auth");
+  
+  useEffect(() => {
+    console.log("Navigation: isLoggedIn:", isLoggedIn);
+    setRouteName(isLoggedIn ? "App" : "Auth");
+  }, [isLoggedIn]);
+
+  console.log("Navigation: Initial route name:", routeName);
+
   return (
-    <RootStack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Auth">
+    <RootStack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName={routeName}
+      key={routeName} // key prop to force re-render on route change
+    >
       <RootStack.Screen name="Auth" component={AuthStack} />
       <RootStack.Screen name="App" component={AppStack} />
     </RootStack.Navigator>
