5bfbdb465903e2f2a51798845c41db3f93b8ee37
Space0code <bozak.tomi@gmail.com>
2025-04-17 13:37:12 +0300

    My Shop navigation added for shop and company (and admin) role

--- SavrApp/app/navigation/AppStack.js
+++ SavrApp/app/navigation/AppStack.js
@@ -9,7 +9,6 @@ import ErrorScreen from "../screens/ErrorScreen";
 import SettingsScreen from "../screens/SettingsScreen";
 import ShopScreen from "../screens/ShopScreen";
 import CompanyListScreen from "../screens/CompanyListScreen";
-import CheckoutScreen from "../screens/CheckoutScreen";
 import CardPaymentScreen from "../screens/CardPaymentScreen";
 
 const Stack = createStackNavigator();
@@ -17,11 +16,15 @@ const Stack = createStackNavigator();
 const AppStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="HomeTabs">
+      {/* Initial route for the app stack is HomeTabs */}
       <Stack.Screen name="HomeTabs" component={HomeTabs} />
+
+      {/* We don't have Main Screen, My Shop, ... and other screen accessible via tabs to be clear that HomeTabs is the root */}
+
+      {/* Other screens */}
       <Stack.Screen name="Settings" component={SettingsScreen} />
       <Stack.Screen name="Shop" component={ShopScreen} />
       <Stack.Screen name="CompanyList" component={CompanyListScreen} />
-      <Stack.Screen name="Checkout" component={CheckoutScreen} />
       <Stack.Screen name="CardPayment" component={CardPaymentScreen} />
       <Stack.Screen name="Error" component={ErrorScreen} />
     </Stack.Navigator>
--- SavrApp/app/navigation/HomeTabs.js
+++ SavrApp/app/navigation/HomeTabs.js
@@ -1,19 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import { SettingsContext } from "../contexts/SettingsContext";
+import { useNavigation } from "@react-navigation/native";
 
 // Screens
 import MainScreen from "../screens/MainScreen";
 import DashboardScreen from "../screens/DashboardScreen";
 import CheckoutScreen from "../screens/CheckoutScreen";
+import ShopScreen from "../screens/ShopScreen";
 
 const Tab = createBottomTabNavigator();
 
 const HomeTabs = () => {
   const { darkMode } = useContext(SettingsContext);
+  const navigation = useNavigation();
 
-  const items = [
+
+  // For debugging purposes, log all route names
+  useEffect(() => {
+    const state = navigation.getState();
+    console.log("All route names:", state.routeNames);
+    // If you need nested routes, inspect state.routes, e.g.:
+    // state.routes.forEach(r => console.log(r.name, r.state?.routeNames));
+  }, [navigation]);
+
+  const dummyItems = [ // TODO: Replace with actual data
     {
       id: 1,
       name: "Surprise Sandwich",
@@ -36,6 +48,42 @@ const HomeTabs = () => {
     }
   ];
 
+  const dummyShop = {
+    "id": 10,
+    "name": "Kahvila konditoria Aino",
+    "description": "Best coffee in town",
+    "address": "Merikoskenkatu 9",
+    "postalCode": "90500",
+    "city": "Oulu",
+    "country": "Finland",
+    "phone": "+385911234567",
+    "email": "kahvila@example.com",
+    "companyId": 9,
+    "latitude": 65.0254999,
+    "longitude": 25.4696293,
+    "dtcreated": "2025-03-30T18:12:38.701Z",
+    "dtupdated": "2025-03-30T18:12:38.701Z",
+    "dtdeleted": null,
+    "rating": 5,
+    "ratings": 5,
+    "dtremoved": null,
+    "primaryCategory": "florist",
+    "images": [
+      {
+        "alt": "Uploaded Image",
+        "url": "/public/images/image-1744310284147-4198645356823.webp",
+        "type": "uploaded"
+      },
+      {
+        "alt": "Uploaded Image",
+        "url": "/public/images/image-1744310323270-0962859202703.webp",
+        "type": "uploaded"
+      }
+    ],
+    "currencyId": "eur",
+    "categories": []
+  }
+
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
@@ -46,6 +94,7 @@ const HomeTabs = () => {
           if (route.name === "Home") iconName = "home";
           else if (route.name === "Dashboard") iconName = "stats-chart";
           else if (route.name === "Checkout") iconName = "cart";
+          else if (route.name === "My Shop") iconName = "storefront";
 
           return <Ionicons name={iconName} size={size} color={color} />;
         },
@@ -57,8 +106,9 @@ const HomeTabs = () => {
       })}
     >
       <Tab.Screen name="Home" component={MainScreen} />
+      <Tab.Screen name="My Shop" component={ShopScreen} initialParams={{shop: dummyShop}} />
       <Tab.Screen name="Dashboard" component={DashboardScreen} />
-      <Tab.Screen name="Checkout" component={CheckoutScreen} initialParams={{ items: items }} />
+      <Tab.Screen name="Checkout" component={CheckoutScreen} initialParams={{ items: dummyItems }} />
     </Tab.Navigator>
   );
 };
