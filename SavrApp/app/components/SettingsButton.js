8909f7961f2e62b29f32347f79ae707f0156e20f
Maskady <florent.tout@gmail.com>
2025-04-22 16:11:59 +0300

    Refactor SettingsButton to use context for dark mo de styles

--- SavrApp/app/components/SettingsButton.js
+++ SavrApp/app/components/SettingsButton.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState, useContext } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import getStyles from '../styles/SettingsStyles';
+import { SettingsContext } from '../contexts/SettingsContext';
 
 export const SettingsButton = () => {
-    const [styles, setStyles] = useState(getStyles());
-
+    const { darkMode } = useContext(SettingsContext);
+    const [styles] = useState(getStyles(darkMode));
     return <Ionicons name="settings-sharp" size={styles.settingsDropDown.settingsIcon.size} color={styles.settingsDropDown.settingsIcon.color} />
 }
