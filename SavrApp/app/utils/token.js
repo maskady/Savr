84147b35a13d72cb8992e3281f83c94024eab12f
Maskady <florent.tout@gmail.com>
2025-04-23 10:59:05 +0300

    Refactor token.js to use default export for getToken and improve formatting

--- SavrApp/app/utils/token.js
+++ SavrApp/app/utils/token.js
@@ -2,37 +2,39 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import request from './request';
 
 export const storeToken = async (token) => {
-    try {
-        await AsyncStorage.setItem('userToken', token);
-    } catch (error) {
-        console.error("Error during token storage:", error);
-    }
+  try {
+      await AsyncStorage.setItem('userToken', token);
+  } catch (error) {
+      console.error("Error during token storage:", error);
+  }
 };
 
-export const getToken = async () => {
-    try {
-        const token = await AsyncStorage.getItem('userToken');
-        return token;
-    } catch (error) {
-        console.error("Error during token retrieval:", error);
-        return null;
-    }
+const getToken = async () => {
+  try {
+      const token = await AsyncStorage.getItem('userToken');
+      return token;
+  } catch (error) {
+      console.error("Error during token retrieval:", error);
+      return null;
+  }
 };
 
 export const removeToken = async () => {
-    try {
-        await AsyncStorage.removeItem('userToken');
-    } catch (error) {
-        console.error("Error during token removal:", error);
-    }
+  try {
+      await AsyncStorage.removeItem('userToken');
+  } catch (error) {
+      console.error("Error during token removal:", error);
+  }
 };
   
 export const refreshToken = async (token) => {
-    try {
-      const response = await request('/auth/refresh-token', 'POST', { token });
-      return response;
-    } catch (error) {
-      console.error("Error in refreshToken:", error);
-      throw error; // Rethrow so the caller can handle it
-    }
-  };
+  try {
+    const response = await request('/auth/refresh-token', 'POST', { token });
+    return response;
+  } catch (error) {
+    console.error("Error in refreshToken:", error);
+    throw error; // Rethrow so the caller can handle it
+  }
+};
+
+export default getToken;
